5f7489621297ad3d3107f67e6297e93edf665f91
skybaile <42541865+skybaile@users.noreply.github.com>
2018-10-07 14:26:56 -0400

    Tables create in SQL

---
+++ app/data/newdata.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE Homework (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  comment VARCHAR(90) NOT NULL,
+);
+
+INSERT INTO Homework (id, comment)
+VALUES (1, 'Comment 1');
+INSERT INTO Homework (id, comment)
+VALUES (2, 'Comment 2');
+INSERT INTO Homework (id, name)
+VALUES (3, 'Comment 3');
+INSERT INTO Homework (id, name)
+VALUES (4, 'Comment 4');
