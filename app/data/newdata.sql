488a4ef54af1296d36899e80889c6006c19ff643
skybaile <42541865+skybaile@users.noreply.github.com>
2018-10-07 15:38:57 -0400

    table updated

--- app/data/newdata.sql
+++ app/data/newdata.sql
@@ -1,14 +1,14 @@
 
 CREATE TABLE Homework (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  comment VARCHAR(90) NOT NULL,
+  comment VARCHAR(90) NOT NULL
 );
 
 INSERT INTO Homework (id, comment)
 VALUES (1, 'Comment 1');
 INSERT INTO Homework (id, comment)
 VALUES (2, 'Comment 2');
-INSERT INTO Homework (id, name)
+INSERT INTO Homework (id, comment)
 VALUES (3, 'Comment 3');
-INSERT INTO Homework (id, name)
+INSERT INTO Homework (id, comment)
 VALUES (4, 'Comment 4');
