bf4d3f14ff94e95d5c19e26cd4f4dcef6d38e956
skybaile <42541865+skybaile@users.noreply.github.com>
2018-10-08 17:40:36 -0400

    syntax error fixed.

--- app/common.php
+++ app/common.php
@@ -3,7 +3,7 @@
 chdir(__DIR__);
 set_include_path (__DIR__);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
+if ($_SERVER['REQUEST_METHOD'] == 'POST'
 && stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
   $_POST = json_decode(file_get_contents('php://input'), true);
 }
