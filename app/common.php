3b5db4d711fc33eb7ee14f3567d83c08d7a46267
skybaile <42541865+skybaile@users.noreply.github.com>
2018-10-08 17:42:11 -0400

    syntax error fixed

--- app/common.php
+++ app/common.php
@@ -7,6 +7,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'
 && stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
   $_POST = json_decode(file_get_contents('php://input'), true);
 }
-require 'environment.php';
+require 'environment.default.php';
 /** MODELS **/
 require 'models/Comment.php';
