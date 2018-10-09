432ec2a6584bb5acf29244c9a5c8a8103763bab6
skybaile <42541865+skybaile@users.noreply.github.com>
2018-10-09 13:41:21 -0400

    changed filename case

---
+++ app/models/Comment.php
@@ -0,0 +1,44 @@
+<?php
+
+class Comment
+{
+  public $id;
+  public $comment;
+
+  public function __construct($data) {
+    $this->id = isset($data['id']) ? intval($data['id']) : null);
+    $this->comment = $data['comment'];
+  }
+  public function create() {
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    $sql = 'INSERT Homework (comment)
+            Values (?)';
+    $statement = $db->prepare($sql);
+    $success = $statement->execute([
+      $this->comment
+
+    ]);
+    $this->id = $db->lastInsertId();
+  }
+
+  public static function fetchAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    // 2. Prepare query
+    $sql = 'SELECT * FROM Homework';
+
+    $statement = $db->prepare($sql);
+
+    // 3. Run query
+    $success = $statement->execute();
+
+    // 4. Handle results
+    $arr = [];
+    while ($data = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $newComment =  new Comment($data);
+      array_push($arr, $newComment);
+    }
+    return $arr;
+  }
+}
