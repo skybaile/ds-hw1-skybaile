6b4ed68d289fce2f7087943c4ece5459af34d02c
skybaile <42541865+skybaile@users.noreply.github.com>
2018-10-15 16:43:27 -0400

    comment.js updated

--- public/js/comment.js
+++ public/js/comment.js
@@ -13,29 +13,18 @@ var commentApp = new Vue({
 
   methods: {
     handleWorkForm(e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      const s = JSON.stringify(this.commentForm);
-      console.log(s);
+    const s = JSON.stringify(this.commentForm);
+    console.log(s);
 
-      // POST to remote server
-      fetch('api/comment.php', {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        headers: {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        body: s // body data type must match "Content-Type" header
-      })
-      .then( response => response.json() )
-      .then( json => {this.comment.push(json)})
-      .catch( err => {
-        console.error('COMMENT POST ERROR:');
-        console.error(err);
-      })
-
-      // Reset workForm
-      this.workForm = this.getEmptyWorkForm();
-    },
+    fetch('api/comment.php', {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: s
+    })
 
     getEmptyCommentForm() {
       return {
